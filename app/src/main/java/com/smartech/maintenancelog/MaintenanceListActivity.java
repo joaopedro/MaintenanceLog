a005f3c5ddb5e3edbb70a95df27807f0db47184b
jpedro <jfpSinfic10>
2014-09-14 20:43:48 +0100

    Initial commit

---
+++ app/src/main/java/com/smartech/maintenancelog/MaintenanceListActivity.java
@@ -0,0 +1,83 @@
+package com.smartech.maintenancelog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+
+/**
+ * An activity representing a list of Maintenances. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link MaintenanceDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link MaintenanceListFragment} and the item details
+ * (if present) is a {@link MaintenanceDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link MaintenanceListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class MaintenanceListActivity extends Activity
+        implements MaintenanceListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenance_list);
+
+        if (findViewById(R.id.maintenance_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((MaintenanceListFragment) getFragmentManager()
+                    .findFragmentById(R.id.maintenance_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link MaintenanceListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(MaintenanceDetailFragment.ARG_ITEM_ID, id);
+            MaintenanceDetailFragment fragment = new MaintenanceDetailFragment();
+            fragment.setArguments(arguments);
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.maintenance_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, MaintenanceDetailActivity.class);
+            detailIntent.putExtra(MaintenanceDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
